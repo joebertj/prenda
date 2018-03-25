d8744c1caa75c0f7916071ee5098d445a865493c
Joebert Jacaba <joebertj@gmail.com>
2018-03-26 02:04:18 +0800

    fix languange selector for openshift

--- src/com/prenda/servlet/LanguageSelector.java
+++ src/com/prenda/servlet/LanguageSelector.java
@@ -27,14 +27,35 @@ public class LanguageSelector {
 			@RequestParam("lang") String lang) {
 		localeResolver.setLocale(request, response, locale);
 		int idx = redirectUrl.indexOf("/prenda/");
+		int idxC = redirectUrl.indexOf("/prenda/common/");
+		/*int idxA = redirectUrl.indexOf("/prenda/admin/");
+		int idxU = redirectUrl.indexOf("/prenda/customer/");
+		int idxO = redirectUrl.indexOf("/prenda/owner/");
+		int idxM = redirectUrl.indexOf("/prenda/manage/");
+		int idxR = redirectUrl.indexOf("/prenda/register/");*/
 		log.info("idx: " + idx);
 		if(idx==0) { //prenda context
-			redirectUrl = redirectUrl.substring(8);
+			if(idxC==0) {
+				redirectUrl = redirectUrl.substring(15); //relative to common
+			/*}else if(idxA==0){
+				redirectUrl = redirectUrl.substring(14);
+			}else if(idxU==0){
+				redirectUrl = redirectUrl.substring(17);
+			}else if(idxO==0){
+				redirectUrl = redirectUrl.substring(14);
+			}else if(idxM==0){
+				redirectUrl = redirectUrl.substring(15);
+			}else if(idxR==0){
+				redirectUrl = redirectUrl.substring(17);*/
+			}else{
+				redirectUrl = redirectUrl.substring(7);
+			}
 		}else if(idx==-1) { //ROOT context
 			;
 		}
+		log.info("redirectUrl: " + redirectUrl);
 		log.info("locale: " + locale);
 		map.addAttribute("lang", lang);
-		return "redirect:/" + redirectUrl;
+		return "redirect:" + redirectUrl;
 	}
 }
